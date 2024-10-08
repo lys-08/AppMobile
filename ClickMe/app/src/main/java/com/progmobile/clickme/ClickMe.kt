58c07375e3584ae9d117cebcd3eed30b5f099250
Lydie Santos <lydie.sts@gmail.com>
2024-10-08 14:45:56 -0400

    Navigation + HomePage + Niveau 1
    Implémentation du système de navigation, du niveau 1, ainsi que du menu du jeu (avec des boutons menant vers les différents niveaux sous forme de liste déroulante)

--- ClickMe/app/src/main/java/com/progmobile/clickme/ClickMe.kt
+++ ClickMe/app/src/main/java/com/progmobile/clickme/ClickMe.kt
@@ -1,2 +1,67 @@
 package com.progmobile.clickme
 
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.progmobile.clickme.ui.HomePage
+import com.progmobile.clickme.ui.levels.Level_01
+
+/*
+ * Enum class containing the pages of the app
+ */
+enum class Screens(@StringRes val title: Int) {
+    HomePage(title = R.string.app_name),
+    Level_01(title = R.string.level_01),
+    Level_02(title = R.string.level_02),
+    Level_03(title = R.string.level_03),
+    Level_04(title = R.string.level_04),
+    Level_05(title = R.string.level_05)
+}
+
+
+@Composable
+fun ClickMeApp(
+    navController: NavHostController = rememberNavController()
+) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = Screens.valueOf(
+        backStackEntry?.destination?.route?: Screens.HomePage.name
+    )
+    Scaffold() {
+            innerPadding ->
+
+        NavHost(
+            navController = navController,
+            startDestination = Screens.HomePage.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = Screens.HomePage.name) {
+                HomePage(
+                    navController = navController,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding((dimensionResource(id = R.dimen.padding_medium)))
+                )
+            }
+            composable(route = Screens.Level_01.name) {
+                Level_01(
+                    navController = navController,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding((dimensionResource(id = R.dimen.padding_medium)))
+                )
+            }
+        }
+    }
+}
--- ClickMe/app/src/main/java/com/progmobile/clickme/ui/HomePage.kt
+++ ClickMe/app/src/main/java/com/progmobile/clickme/ui/HomePage.kt
@@ -1,2 +1,109 @@
 package com.progmobile.clickme.ui
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.progmobile.clickme.R
+import com.progmobile.clickme.Screens
+
+
+/**
+ * Composable that allows the user to select the desired action to do and triggers
+ * the navigation to next screen
+ */
+@Composable
+fun HomePage(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        // Title
+        Text(
+            text = stringResource(id = R.string.app_name),
+            style = MaterialTheme.typography.displayLarge,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp),
+            textAlign = TextAlign.Center
+        )
+
+        val levels = listOf(
+            Pair(R.string.level_01, Screens.Level_01.name),
+            Pair(R.string.level_02, Screens.Level_02.name),
+            Pair(R.string.level_03, Screens.Level_03.name),
+            Pair(R.string.level_04, Screens.Level_04.name)
+        )
+
+        // Levels button
+        Column {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier.fillMaxWidth().fillMaxHeight()
+            ) {
+                items(levels) { level ->
+                    LevelButton(
+                        labelResourceId = level.first,
+                        onClick = { navController.navigate(level.second) }
+                    )
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Customizable button composable that displays the [labelResourceId]
+ * and triggers [onClick] lambda when this composable is clicked
+ */
+@Composable
+fun LevelButton(
+    @StringRes labelResourceId: Int,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.widthIn(min = 250.dp)
+    ) {
+        Text(stringResource(labelResourceId))
+    }
+}
+
+
+@Preview
+@Composable
+fun StartHomePagePreview() {
+    MaterialTheme {
+        HomePage(
+            navController = rememberNavController(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(dimensionResource(R.dimen.padding_medium))
+        )
+    }
+}
