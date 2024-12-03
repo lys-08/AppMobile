d525c1cd418c57020bccdd9c73f57d2ea7ca9c83
Lydie Santos <lydie.sts@gmail.com>
2024-12-03 17:37:38 -0500

    Fix boîte de dialogue pour les permissions

--- ClickMe/app/src/main/java/com/progmobile/clickme/ClickMe.kt
+++ ClickMe/app/src/main/java/com/progmobile/clickme/ClickMe.kt
@@ -7,14 +7,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.progmobile.clickme.data.DataSource.levelsMap
 import com.progmobile.clickme.ui.ClickMeBottomBar
@@ -53,51 +50,20 @@ enum class Screens(@StringRes val title: Int) {
  * Application composable, calls the Home Page, setups the Bottom Bar and the routes for the levels
  * @param navController the navigation controller, could be passed from MainActivity but currently not used that way
  * @param mainActivityInstance the instance of MainActivity, passed from MainActivity to override the back button behaviour, to always go back to HomePage
- * @param permissionsDenied state to check if at least one permission have been denied, currently unused
  */
 @Composable
 fun ClickMeApp(
-    permissionsDenied : MutableState<Boolean>,
     navController: NavHostController = rememberNavController(),
     mainActivityInstance: MainActivity
 ) {
-    // ================= PERMISSION =================
-    /*var showDialog by remember { mutableStateOf(false) }
-
-    // An Alert message is print if at least one permission have been denied
-    // TODO : fix
-    LaunchedEffect(!permissionsDenied.value) {
-        if (!permissionsDenied.value) {
-            showDialog = true
-        }
-    }
-
-    if (showDialog) {
-        AlertDialog(
-            onDismissRequest = { showDialog = false },
-            title = { Text("Permission denied") },
-            text = { Text("Some permission have been denied. WARNING : you may nit complete some level, please accept the permission.") },
-            confirmButton = {
-                Button(onClick = { showDialog = false }) {
-                    Text("OK")
-                }
-            }
-        )
-    }*/
-    // =============== END PERMISSION ===============
-
     // Override callback behaviour to always go back to HomePage
-    mainActivityInstance.onBackPressedDispatcher.addCallback() {
+    mainActivityInstance.onBackPressedDispatcher.addCallback {
         // Handle the back button event
         navController.navigate(Screens.HomePage.name) {
             popUpTo(0) // Clear the back stack
         }
     }
 
-    val backStackEntry by navController.currentBackStackEntryAsState()
-    val currentScreen = Screens.valueOf(
-        backStackEntry?.destination?.route?: Screens.HomePage.name
-    )
     Scaffold(
         bottomBar = {
             ClickMeBottomBar(
